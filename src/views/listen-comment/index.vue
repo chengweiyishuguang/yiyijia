fb6b823737c42ca4e3330e9cdcff7d0ff50e004f
chengweiyishuguang <984425247@qq.com>
2020-05-31 13:24:04 +0800

    听书-评论-输入框

--- src/views/listen-comment/index.vue
+++ src/views/listen-comment/index.vue
@@ -69,7 +69,7 @@
           <div class="right-bo">
             <span>
               <i class="weidianzan" style="display: none;"></i>
-              <i class="dianzan" ></i>
+              <i class="dianzan"></i>
               416
             </span>
             <span>
@@ -80,6 +80,10 @@
         </div>
       </li>
     </ul>
+    <!-- 留言输入框 -->
+    <div class="last-fixed">
+      <textarea name id placeholder="想评论点什么呢"></textarea>
+    </div>
   </div>
 </template>
 
@@ -137,115 +141,126 @@ export default {
     color: rgba(85, 85, 85, 1);
   }
 }
- ul {
-   padding:0 34px 0 34px;
- }
-  li {
-     width: 100%;
-     height: 100%;
-    display: flex;
+ul {
+  padding: 0 34px 0 34px;
+}
+li {
+  width: 100%;
+  height: 100%;
+  display: flex;
 
-     overflow: hidden;
-
+  overflow: hidden;
+}
+.comment-left {
+  width: 68px;
+  height: 68px;
+  border-radius: 50%;
+  background-color: #ccc;
+  flex-shrink: 0;
+  img {
+    width: 100%;
+    height: 100%;
   }
-   .comment-left {
+}
+.comment-right {
+  margin-left: 8px;
 
-     width: 68px;
-     height: 68px;
-     border-radius: 50%;
-      background-color: #ccc;
-      flex-shrink: 0;
-      img {
-        width: 100%;
-        height: 100%;
+  .right-top {
+    line-height: 30px;
+    p {
+      font-size: 30px;
+      font-family: Source Han Sans SC;
+      font-weight: 700;
+      color: rgba(18, 18, 18, 1);
+    }
+    span {
+      font-size: 20px;
+      font-family: Source Han Sans SC;
+      font-weight: 500;
+      color: rgba(153, 153, 153, 1);
+    }
+  }
 
+  .right-text {
+    margin-top: 29px;
+    font-size: 26px;
+    font-family: Source Han Sans SC;
+    font-weight: 400;
+    color: rgba(102, 102, 102, 1);
+    line-height: 42px;
+  }
+  span {
+    font-size: 20px;
+    font-family: Source Han Sans SC;
+    font-weight: 500;
+    color: rgba(153, 153, 153, 1);
+  }
+
+  .right-bo {
+    text-align: right;
+
+    span {
+      font-size: 22px;
+      font-family: Source Han Sans SC;
+      font-weight: 400;
+      color: rgba(85, 85, 85, 1);
+      position: relative;
+    }
+    span:nth-child(1) {
+      .weidianzan {
+        background: url(../../assets/zan1.png) no-repeat;
+        width: 31px;
+        height: 33px;
+        background-size: 31px;
+        position: absolute;
+        top: -2px;
+        left: -40px;
+      }
+      .dianzan {
+        background: url(../../assets/zan2.png) no-repeat;
+        width: 31px;
+        height: 33px;
+        background-size: 31px;
+        position: absolute;
+        top: -2px;
+        left: -40px;
       }
     }
-    .comment-right {
-
-      margin-left:8px;
-
-      .right-top {
-        line-height: 30px;
-        p {
-          font-size:30px;
-          font-family:Source Han Sans SC;
-          font-weight:700;
-          color:rgba(18,18,18,1);
-
-        }
-        span {
-          font-size:20px;
-          font-family:Source Han Sans SC;
-          font-weight:500;
-          color:rgba(153,153,153,1);
-        }
+    span:nth-child(2) {
+      margin-left: 44px;
+      .liuyan {
+        background: url(../../assets/liuyan.png) no-repeat;
+        width: 31px;
+        height: 33px;
+        background-size: 31px;
+        position: absolute;
+        top: -2px;
+        left: -40px;
       }
-
-     .right-text {
-        margin-top:29px;
-        font-size:26px;
-        font-family:Source Han Sans SC;
-        font-weight:400;
-        color:rgba(102,102,102,1);
-        line-height:42px;
-
-      }
-      span {
-        font-size: 20px;
-        font-family: Source Han Sans SC;
-        font-weight: 500;
-        color: rgba(153, 153, 153, 1);
-      }
-
-      .right-bo {
-        text-align: right;
-
-        span {
-          font-size: 22px;
-          font-family: Source Han Sans SC;
-          font-weight: 400;
-          color: rgba(85, 85, 85, 1);
-          position: relative;
-        }
-        span:nth-child(1) {
-          .weidianzan{
-            background: url(../../assets/zan1.png) no-repeat;
-            width: 31px;
-            height: 33px;
-            background-size: 31px;
-            position: absolute;
-            top: -2px;
-            left: -40px;
-          }
-          .dianzan {
-
-            background: url(../../assets/zan2.png) no-repeat;
-            width: 31px;
-            height: 33px;
-            background-size: 31px;
-            position: absolute;
-            top: -2px;
-            left: -40px;
-
-          }
-        }
-          span:nth-child(2) {
-             margin-left:44px;
-            .liuyan{
-
-            background: url(../../assets/liuyan.png) no-repeat;
-            width: 31px;
-            height: 33px;
-            background-size: 31px;
-            position: absolute;
-            top: -2px;
-            left: -40px;
-          }
-          }
-
-      }
-
     }
+  }
+}
+.last-fixed {
+  width: 750px;
+  height: 98px;
+  line-height: 98px;
+  background: rgba(255, 255, 255, 1);
+  box-shadow: 0px 0px 13px 0px rgba(0, 0, 0, 0.18);
+  box-sizing: border-box;
+  text-align: center;
 
+  textarea {
+    border: 0;
+    padding-left: 35px;
+    width: 552px;
+    height: 61px;
+    line-height: 61px;
+    background: rgba(242, 242, 242, 1);
+    border-radius: 20px;
+    resize: none;
+    font-size: 24px;
+    color: rgba(136, 136, 136, 1);
+    box-sizing: border-box;
+  }
+}
 </style>
